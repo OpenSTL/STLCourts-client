d90167dff005f6ad07ac27e39b439b0e7ae2c76e
Alex White <alex.white@daugherty.com>
2015-09-09 19:59:23 -0500

    Initial scaffolded angular app via yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ghAngularApp
+ * @description
+ * # ghAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ghAngularApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ghAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ghAngularApp
+ */
+angular.module('ghAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
