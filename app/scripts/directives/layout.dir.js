86e043173bc712b27c01add01f0fc388b611c146
Alex White <alex.white@daugherty.com>
2015-09-13 10:41:02 -0500

    Polish and tweaks

--- app/scripts/directives/layout.dir.js
+++ app/scripts/directives/layout.dir.js
@@ -5,14 +5,13 @@ angular.module('ghAngularApp').directive('layout', function () {
     restrict: 'A',
 
     link: function ($scope, element) {
-      $scope.$on('$stateChangeStart',
-        function(event, toState, toParams, fromState, fromParams){
-          if(toState.name === 'home') {
-            element.addClass('background');
-          } else {
-            element.removeClass('background');
-          }
-        })
+      $scope.$on('$stateChangeStart', function(event, toState){
+        if(toState.name === 'home') {
+          element.addClass('background');
+        } else {
+          element.removeClass('background');
+        }
+      });
     }
   };
 });
