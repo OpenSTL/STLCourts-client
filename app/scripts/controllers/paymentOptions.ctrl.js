057ccac6f5dcd6041f6f1fbf5f536b131a8929fa
Alex White <alex.white@daugherty.com>
2015-09-13 11:31:15 -0500

    More styling of payment options

--- app/scripts/controllers/paymentOptions.ctrl.js
+++ app/scripts/controllers/paymentOptions.ctrl.js
@@ -1,12 +1,15 @@
 'use strict';
 
-angular.module('ghAngularApp').controller('PaymentOptionsCtrl', function (citation, Opportunities) {
+angular.module('ghAngularApp').controller('PaymentOptionsCtrl', function (citation, Opportunities, $state) {
+  var ctrl = this;
 
-    var ctrl = this;
+  ctrl.citation = citation;
 
-    ctrl.citation = citation;
+  Opportunities.findByCourtId(citation.court_id).then(function (results) {
+    ctrl.opportunities = results;
+  });
 
-    Opportunities.findByCourtId(citation.court_id).then(function(results) {
-       ctrl.opportunities = results;
-    });
+  ctrl.goBack = function(){
+    $state.go('citationInfo', {citations : [ctrl.citation]});
+  }
 });
