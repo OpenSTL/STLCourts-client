09939e9e838259e27c98812b0da37ef66ab7403a
matt598 <matt598@att.net>
2016-07-09 13:53:26 -0500

    Adding Moment

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angularjs-toaster/toaster.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-ui-select/dist/select.js',
+      'bower_components/moment/moment.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/vendor/jcs-auto-validate.min.js',
