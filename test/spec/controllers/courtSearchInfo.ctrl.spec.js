8c0e1025d415a7b9a8df2a3c5cdbb46cf4405b00
ctwoolsey <clark.woolsey@gmail.com>
2016-10-14 20:26:50 -0500

    Created Spec Tests for CourtSearchInfo and the Home Controllers

---
+++ test/spec/controllers/courtSearchInfo.ctrl.spec.js
@@ -0,0 +1,58 @@
+'use strict';
+
+describe('CourtSearchInfoCtrl', function() {
+  var CourtSearchInfoCtrl;
+  var sample_court;
+
+  sample_court = {
+    id: 9,
+    latitutde: 38.7947506,
+    longitude: -90.26261696,
+    municipality: "Black Jack",
+    address: "12500 Old Jamestown Road",
+    city: "St. Louis",
+    state: "MO",
+    zip_code: 63033
+  };
+
+  beforeEach(function() {
+    module('yourStlCourts');
+
+    inject(function($controller,$state, $window, $q, $httpBackend){
+
+      CourtSearchInfoCtrl = $controller('CourtSearchInfoCtrl',{
+        $state: $state,
+        $window: $window,
+        court:sample_court
+      });
+
+      $httpBackend.whenGET(/courts/).respond(200, '');
+    });
+  });
+
+  xit('goes back to home page if court is null',inject(function($state){
+    spyOn($state,'go');
+    sample_court = null;
+    expect($state.go).toHaveBeenCalledWith('home');
+  }));
+
+  it('prints if printCourtInfo is called',inject(function($window){
+    spyOn($window,'print');
+    CourtSearchInfoCtrl.printCourtInfo();
+    expect($window.print).toHaveBeenCalled();
+  }));
+
+  it('sets court on initialization',inject(function($rootScope){
+    $rootScope.$apply();
+    expect(CourtSearchInfoCtrl.courtInfo).toEqual(sample_court);
+  }));
+
+  it('calls and gets correct directions',(function(){
+    //spyOn(CourtSearchInfoCtrl,'getCourtDirectionLink');
+    var directionString = "https://maps.google.com?saddr=Current+Location&daddr=12500+Old Jamestown Road+St. Louis+MO+63033";
+    //expect(CourtSearchInfoCtrl.getCourtDirectionLink).toHaveBeenCalled();//.and.toEqual(directionString);
+    expect(CourtSearchInfoCtrl.courtDirectionLink).toEqual(directionString);
+
+  }));
+
+});
