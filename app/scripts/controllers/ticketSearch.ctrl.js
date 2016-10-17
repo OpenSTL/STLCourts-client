0648bc600054fedc17568101201842f4d2c45cd8
ctwoolsey <clark.woolsey@gmail.com>
2016-10-17 12:16:39 -0500

    Text update for ticket not found

--- app/scripts/controllers/ticketSearch.ctrl.js
+++ app/scripts/controllers/ticketSearch.ctrl.js
@@ -95,10 +95,11 @@ angular.module('yourStlCourts').controller('ticketSearchCtrl', function (Citatio
     }
 
     Citations.find(params).then(function(result){
+      var noTicketsFoundMsg = 'We could not find any results for the  information you provided. It\'s possible that the municipality that issued your citation does not participate in YourSTLCourts. You may obtain information for any municipality via [link to search page]. Mention you\'d like them to participate in YourSTLCourts.';
       if(result.citations.length > 0) {
         $state.go('citationInfo', {citations: result.citations});
       } else {
-        toaster.pop('error', 'Oh no! There were no tickets matching your criteria');
+        toaster.pop('error', noTicketsFoundMsg);
       }
     }, function(){
       toaster.pop('error', 'Oh no! We couldn\'t get your ticket information!');
