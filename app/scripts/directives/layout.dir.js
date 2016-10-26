3014249e4e021e4cf2c9562d5f27127030c66002
ctwoolsey <clark.woolsey@gmail.com>
2016-10-25 19:45:32 -0500

    Cleaned up long if line and made it an array to search.

--- app/scripts/directives/layout.dir.js
+++ app/scripts/directives/layout.dir.js
@@ -6,7 +6,13 @@ angular.module('yourStlCourts').directive('layout', function () {
 
     link: function ($scope, element) {
       $scope.$on('$stateChangeStart', function(event, toState){
-        if (toState.name === 'home' || toState.name === 'ticketSearch' || toState.name === 'about' || toState.name === 'help' || toState.name === 'info' || toState.name === 'error') {
+        var statesToUseBackground = ['home',
+                                     'ticketSearch',
+                                     'about',
+                                     'help',
+                                     'info',
+                                     'error'];
+        if (statesToUseBackground.indexOf(toState.name) != -1){
           element.addClass('background');
         } else {
           element.removeClass('background');
