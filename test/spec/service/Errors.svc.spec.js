c2c49d3794a41173fbdca5e521d7cd0311796829
ctwoolsey <clark.woolsey@gmail.com>
2016-10-27 21:04:10 -0500

    Errors Unit Test

--- test/spec/service/Errors.svc.spec.js
+++ test/spec/service/Errors.svc.spec.js
@@ -9,7 +9,7 @@ describe('Errors', function() {
       Errors = _Errors_;
     });
   });
-  
+
   it('returns the correct error object',function(){
     var error = Errors.makeError(404,"Got an error.");
     expect(error.code).toEqual(404);
