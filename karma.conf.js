49a7cb25ac2fe334ed434c10f1fbcc435ed8ca17
ctwoolsey <clark.woolsey@gmail.com>
2017-02-02 12:25:05 -0600

    Removed ESRI references (#43)

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,6 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/angular-esri-map/dist/angular-esri-map.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angularjs-toaster/toaster.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
