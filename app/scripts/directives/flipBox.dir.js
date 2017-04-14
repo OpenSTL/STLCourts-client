eba6edf1f6c661d46c204b6bd149c0736b42ea4f
ctwoolsey <clark.woolsey@gmail.com>
2017-04-14 16:40:20 -0500

    intermediate commit

--- app/scripts/directives/flipBox.dir.js
+++ app/scripts/directives/flipBox.dir.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('yourStlCourts').directive('flipBox', function () {
+angular.module('yourStlCourts').directive('flipBox', function ($window) {
   return {
     restrict: 'E',
     scope:{
@@ -13,11 +13,35 @@ angular.module('yourStlCourts').directive('flipBox', function () {
     link: function ($scope, element) {
       var scope = $scope;
       scope.ctrl.faqBoxFront = scope.front;
-      scope.$apply();
+      scope.ctrl.faqBoxBack = scope.front;
+      scope.ctrl.frontShowing = true;
+     // scope.$apply();
 
 
       $(element).on('click',function(){
+        if (scope.ctrl.frontShowing){
+          scope.ctrl.frontShowing = false;
+        }else{
+          scope.ctrl.frontShowing = true;
+        }
+      });
 
+      angular.element($window).on('resize',function(){
+        resizeColumns();
+      });
+      resizeColumns(); //call it for initial setup
+
+      function resizeColumns(){
+        var newWidth = $(window).width()/2 - 75;
+        console.log(newWidth);
+        $(element).find(".flip-box-front,.flip-box-back").each(function(){
+          $(this).width(Number(newWidth));
+        });
+      }
+
+      element.on('$destroy',function(){
+        element.off('click');
+        angular.element($window).off('resize');
       });
     }
   };
