aa8c0e3fb4bb48a44c832d4b9c4ba137c1c0cea5
ctwoolsey <clark.woolsey@gmail.com>
2017-04-17 14:48:53 -0500

    Implemented flipping boxes

--- app/scripts/directives/flipBox.dir.js
+++ app/scripts/directives/flipBox.dir.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('yourStlCourts').directive('flipBox', function ($window) {
+angular.module('yourStlCourts').directive('flipBox', function ($window,$rootScope) {
   return {
     restrict: 'E',
     scope:{
@@ -13,35 +13,43 @@ angular.module('yourStlCourts').directive('flipBox', function ($window) {
     link: function ($scope, element) {
       var scope = $scope;
       scope.ctrl.faqBoxFront = scope.front;
-      scope.ctrl.faqBoxBack = scope.front;
+      scope.ctrl.faqBoxBack = scope.back;
       scope.ctrl.frontShowing = true;
-     // scope.$apply();
+
+      var frontElement;
+      var backElement;
+      var flipContainer;
+
+      $(element).find(".flip-box-front").each(function(){
+        frontElement = this;
+      });
+      $(element).find(".flip-box-back").each(function(){
+        backElement = this;
+      });
+      $(element).find(".flip-box-container").each(function(){
+        flipContainer = this;
+      });
+
 
 
       $(element).on('click',function(){
         if (scope.ctrl.frontShowing){
           scope.ctrl.frontShowing = false;
+          $(frontElement).addClass("HideFront");
+          $(backElement).removeClass("HideBack");
+          $(flipContainer).addClass("flipContainerShowingBack");
         }else{
           scope.ctrl.frontShowing = true;
+          $(frontElement).removeClass("HideFront");
+          $(backElement).addClass("HideBack");
+          $(flipContainer).removeClass("flipContainerShowingBack");
         }
-      });
 
-      angular.element($window).on('resize',function(){
-        resizeColumns();
-      });
-      resizeColumns(); //call it for initial setup
 
-      function resizeColumns(){
-        var newWidth = $(window).width()/2 - 75;
-        console.log(newWidth);
-        $(element).find(".flip-box-front,.flip-box-back").each(function(){
-          $(this).width(Number(newWidth));
-        });
-      }
+      });
 
       element.on('$destroy',function(){
         element.off('click');
-        angular.element($window).off('resize');
       });
     }
   };
