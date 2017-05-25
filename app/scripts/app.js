9960387ebc6ae968d08b35271f7d739b497e443c
ctwoolsey <clark.woolsey@gmail.com>
2017-05-25 09:30:41 -0500

    url address updated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -136,7 +136,7 @@ angular.module('yourStlCourts').config(function ($stateProvider, $urlRouterProvi
       }
     })
     .state('citations', {
-      url: '/citations/{citationId}',
+      url: '/ticketsFromSMS/{citationId}',
       templateUrl: 'views/citations.html',
       controller: 'CitationsCtrl as ctrl',
       resolve: {
