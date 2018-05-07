e2df649dd520feb0700353548946550e7fb570c9
ctwoolsey <clark.woolsey@gmail.com>
2018-05-07 14:47:51 -0500

    unit test for header component completed

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-/// <reference types="karma-viewport" />
-
 module.exports = function (config) {
   config.set({
     basePath: '',
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -5,6 +5,8 @@ import {MatIconModule, MatMenuModule} from '@angular/material';
 import {RouterLinkStubDirective} from '../../testing/router-stubs';
 import {By} from '@angular/platform-browser';
 
+declare var viewport: any;
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
@@ -40,90 +42,100 @@ describe('HeaderComponent', () => {
     expect(logoLink.navigatedTo).toBe('/');
   });
 
-  it('should have dtNavMenu link to correct urls', () => {
-    const linkDes = fixture.debugElement.queryAll(By.css('#dtMenu a'));
-    const links = linkDes.map(del => del.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
+  viewport.from('screen', name => {
+    it('should have dtNavMenu link to correct urls', () => {
+      const linkDes = fixture.debugElement.queryAll(By.css('#dtMenu a'));
+      const links = linkDes.map(del => del.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
 
-    expect( linkDes.length).toBe(6);
+      expect(linkDes.length).toBe(6);
 
-    let linkDe = linkDes[0];
-    let link = links[0];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('');
+      let linkDe = linkDes[0];
+      let link = links[0];
+      linkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(link.navigatedTo).toBe('');
 
-    linkDe = linkDes[1];
-    link = links[1];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/findCourts');
+      linkDe = linkDes[1];
+      link = links[1];
+      linkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(link.navigatedTo).toBe('/findCourts');
 
-    linkDe = linkDes[2];
-    link = links[2];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/findTickets');
+      linkDe = linkDes[2];
+      link = links[2];
+      linkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(link.navigatedTo).toBe('/findTickets');
 
-    linkDe = linkDes[3];
-    link = links[3];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/goingToCourt');
+      linkDe = linkDes[3];
+      link = links[3];
+      linkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(link.navigatedTo).toBe('/goingToCourt');
 
-    linkDe = linkDes[4];
-    link = links[4];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/communityService');
+      linkDe = linkDes[4];
+      link = links[4];
+      linkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(link.navigatedTo).toBe('/communityService');
 
-    linkDe = linkDes[5];
-    link = links[5];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/help');
+      linkDe = linkDes[5];
+      link = links[5];
+      linkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(link.navigatedTo).toBe('/help');
+    });
   });
 
-  it('should have mobileNavMenu link to correct urls', () => {
-    viewport.set('mobile');
-    const linkDes = fixture.debugElement.queryAll(By.css('#mobileMenuButtons button'));
-    const links = linkDes.map(del => del.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
 
-    expect( linkDes.length).toBe(6);
+  viewport.to('mobile', name => {
+    it('should have mobileNavMenu link to correct urls', () => {
+      const menuButton = fixture.debugElement.query(By.css('#mobileMenu button')).nativeElement;
+      menuButton.click();
+      fixture.detectChanges();
 
-    let linkDe = linkDes[0];
-    let link = links[0];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('');
+      fixture.whenStable().then( () => {
+        const linkDes = fixture.debugElement.queryAll(By.css('button'));
+        const links = linkDes.map(del => del.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
 
-    linkDe = linkDes[1];
-    link = links[1];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/findCourts');
+        expect(linkDes.length).toBe(6);
 
-    linkDe = linkDes[2];
-    link = links[2];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/findTickets');
+        let linkDe = linkDes[0];
+        let link = links[0];
+        linkDe.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(link.navigatedTo).toBe('');
 
-    linkDe = linkDes[3];
-    link = links[3];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/goingToCourt');
+        linkDe = linkDes[1];
+        link = links[1];
+        linkDe.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(link.navigatedTo).toBe('/findCourts');
 
-    linkDe = linkDes[4];
-    link = links[4];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/communityService');
+        linkDe = linkDes[2];
+        link = links[2];
+        linkDe.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(link.navigatedTo).toBe('/findTickets');
 
-    linkDe = linkDes[5];
-    link = links[5];
-    linkDe.triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(link.navigatedTo).toBe('/help');
+        linkDe = linkDes[3];
+        link = links[3];
+        linkDe.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(link.navigatedTo).toBe('/goingToCourt');
+
+        linkDe = linkDes[4];
+        link = links[4];
+        linkDe.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(link.navigatedTo).toBe('/communityService');
+
+        linkDe = linkDes[5];
+        link = links[5];
+        linkDe.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(link.navigatedTo).toBe('/help');
+      });
+    });
   });
 });
