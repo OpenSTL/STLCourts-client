b626dc4cd6bc2473952c6346921743f60bad7afe
ctwoolsey <clark.woolsey@gmail.com>
2018-05-21 13:27:10 -0500

    ticket not found component

--- src/app/ticket-not-found/ticket-not-found.component.ts
+++ src/app/ticket-not-found/ticket-not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MunicipalitiesService} from '../services/municipalities.service';
+import {Municipality} from '../models/municipality';
 
 @Component({
   selector: 'app-ticket-not-found',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketNotFoundComponent implements OnInit {
 
-  constructor() { }
+  supportedMunicipalities: Municipality[] = [];
+
+  constructor(private municipalityService: MunicipalitiesService) { }
 
   ngOnInit() {
+    this.municipalityService.findSupported(true).subscribe((supportedMunis) => {
+      this.supportedMunicipalities = supportedMunis;
+    });
   }
-
 }
