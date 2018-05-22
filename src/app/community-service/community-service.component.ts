9a603fddcc53933c3b0d7e1d5cb30b0e85da58be
ctwoolsey <clark.woolsey@gmail.com>
2018-05-22 08:29:11 -0500

    Cleaned up code

--- src/app/community-service/community-service.component.ts
+++ src/app/community-service/community-service.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-community-service',
   templateUrl: './community-service.component.html',
   styleUrls: ['./community-service.component.scss']
 })
-export class CommunityServiceComponent implements OnInit {
+export class CommunityServiceComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
