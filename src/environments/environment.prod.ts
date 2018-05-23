f83280b3100ce5a071994d75f13615221d5801c0
ctwoolsey <clark.woolsey@gmail.com>
2018-05-23 00:50:42 -0500

    Proxy api (#180)
    * proxy settings and environment settings
    
    * api endpoint configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://yourstlcourts.com/api'
+  baseUrl: '/api'
 };
--- src/environments/environment.testLocal.ts
+++ src/environments/environment.testLocal.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  baseUrl: '/api'
+  baseUrl: 'http://test.yourstlcourts.com/api'
 };
