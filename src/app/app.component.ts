25d610c0d05f44ed809b0d6ec69a95edd05e4e07
ctwoolsey <clark.woolsey@gmail.com>
2018-06-18 11:15:16 -0500

    Adding google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {NavigationEnd, Router} from '@angular/router';
+import {} from '@types/google.analytics';
+
+// https://stackoverflow.com/questions/45758852/angular-4-using-google-analytics
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  public constructor(private titleService: Title ) { }
+export class AppComponent implements OnInit {
+  constructor(private titleService: Title,
+              public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
 
   public setTitle( newTitle: string) {
     this.titleService.setTitle( newTitle );
